0a87fab5a40180d1df85c1dc64da6eb090aa5de4
annaemerick <anna.emerick@gmail.com>
2025-03-27 18:35:10 +0000

    cprFinal

---
+++ converting.h
@@ -0,0 +1,10 @@
+
+#ifndef _CONVERTING_H_ 
+#define _CONVERTING_H_
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+void converting (void);
+#endif
+
+void converting(void);
---
+++ convertingMain.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "converting.h"
+
+int main(void) {
+    converting();
+    return 0;
+}
